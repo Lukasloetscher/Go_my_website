9ec9bbed2b83cfb25957dd5e3764c8bf35c8ea83
Lukas Lötcher <lukas.d.loetscher@gmail.com>
2025-02-03 20:56:37 +0100

    writing some output, next  i will uise the stuff from the tuutorial to clean this up, and make it "better"

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi"
+)
+
 func main() {
 
+	mux := chi.NewRouter()
+	mux.Get("/", func(w http.ResponseWriter, r *http.Request) { w.Write([]byte("usghskjghsehg")) })
+
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+
+	err := srv.ListenAndServe()
+	log.Fatal(err)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Lukasloetscher/Go_my_website
 
 go 1.23.5
+
+require github.com/go-chi/chi v1.5.5
