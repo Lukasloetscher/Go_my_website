7e825db4f662764783595b58ed6c03066d9743e5
Lukas Lötcher <lukas.d.loetscher@gmail.com>
2025-02-15 19:37:22 +0100

    added validation of prot number using regex

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,16 +4,33 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"regexp"
 
 	"github.com/go-chi/chi"
 )
 
+// Check_If_Valid_Port checks if the entry is a valid port umber, i.e. it needs to be numeric.
+// Currently we do not test, wheter it is a reasonable port number
+// TODO move into a proper package...
+func Check_If_Valid_Port(port string) (ok bool) {
+	matched, err := regexp.MatchString("[0-9]+", port)
+	if err != nil {
+		return matched
+	} else {
+		panic(err)
+	}
+}
+
 // Get_Port_From_Env() gets thed port from the envirement. Used for docker.
 // TODO move into a proper package...
-func Get_Port_From_Env() string {
-	val, ok := os.LookupEnv("PORT")
+func Get_Port_From_Env() (port string) {
+	port, ok := os.LookupEnv("PORT")
 	if ok {
-		return val
+		if Check_If_Valid_Port(port) {
+			return port
+		} else {
+			panic("No Valid Portnumber was entered.")
+		}
 	} else {
 		return "8080" //default case, if the port is not defined in the envirement.
 	}
@@ -24,7 +41,7 @@ func main() {
 	mux := chi.NewRouter()
 	mux.Get("/", func(w http.ResponseWriter, r *http.Request) { w.Write([]byte("usghskjghsehg")) })
 
-	Port := Get_Port_From_Env()
+	Port := Get_Port_From_Env() //Todo we later need to handle panics!
 
 	srv := &http.Server{
 		Addr:    ":" + Port,
