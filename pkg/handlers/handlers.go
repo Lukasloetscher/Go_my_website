021ada35398510e0c13a71f6022c5c6786030317
Lukas Lötcher <lukas.d.loetscher@gmail.com>
2025-02-19 11:06:37 +0100

    added some generic content, mostly to make sure that we can see that somethign si written.

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -1,6 +1,8 @@
 package handlers
 
 import (
+	"net/http"
+
 	"github.com/Lukasloetscher/Go_my_website/pkg/config/appconfig"
 	"github.com/go-chi/chi"
 )
@@ -8,6 +10,12 @@ import (
 // Add_Manual_Handlers()
 // adds the handlers to the code, which i write manually
 func Add_Manual_Handlers(app_ptr *appconfig.AppConfig, mux *chi.Mux) error {
+
+	//For the moment, i just add a simple palcehodler, so we can still see something:
+	mux.Get("", func(w http.ResponseWriter, r *http.Request) {
+		//Here we need to render the page...
+		w.Write([]byte("Hallo Welt"))
+	})
 
 	return nil
 }
