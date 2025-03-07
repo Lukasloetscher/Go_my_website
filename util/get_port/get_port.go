3b4388c2f58b39101e713908e602c749aeefc9ee
Lukas Lötcher <lukas.d.loetscher@gmail.com>
2025-03-07 16:22:12 +0100

    added comment to port

--- util/get_port/get_port.go
+++ util/get_port/get_port.go
@@ -23,6 +23,7 @@ const port_env string = "PORT"
 
 // Get_Port_From_Env() gets the port from the environment. Used for docker.
 // When the env variable is not set, it defaults to 8080
+// due tio the way ports are handled in docker, it does not make sense to use this. we need to expose the prot anyway in the dockerfile...
 func Get_Port_From_Env() (port string) {
 	port, ok := os.LookupEnv(port_env)
 	if ok {
